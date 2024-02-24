3c8232ec159d00f08da0ebd8e383fa0d27069c61
Andrey <aazhiyanov@gmail.com>
2024-02-24 16:34:46 +0300

    CustomerModel

---
+++ OnlineStore.Models/CustomerModel.cs
@@ -0,0 +1,31 @@
+﻿namespace OnlineStore.Models
+{
+    public class CustomerModel
+    {
+       
+            public int Customerid { get; set; }
+
+            public string Lastname { get; set; } = null!;
+
+            public string Firstname { get; set; } = null!;
+
+            public string? Firdname { get; set; }
+
+            public string? Phone { get; set; }
+
+            public CustomerModel() { }
+
+            public CustomerModel(string lastName, string firstname, string thirdName, string phone)
+            {
+
+                Lastname = lastName;
+                Firstname = firstname;
+                Firdname = thirdName;
+                Phone = phone;
+
+
+            }
+
+        }
+    }
+}
